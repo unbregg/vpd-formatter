cb78adc94cd38e0c9447d0e02773b663b5bdb389
zhangqixiong <zhangqixiong@meituan.com>
2020-08-13 11:10:06 +0800

    feat: 转换后，更新import

--- src/transformer.js
+++ src/transformer.js
@@ -1,5 +1,7 @@
 const ts = require('typescript');
 const traverser = require('./traverser');
+const lodash = require('lodash');
+
 const lifecycleHooks = [
   'beforeCreate',
   'created',
@@ -18,6 +20,8 @@ const supportedPropNames = [
   'watch',
   ...lifecycleHooks,
 ];
+const supportedDecoratorNames = ['Watch', 'Prop'];
+
 // 找到Component装饰器
 function findComponentDecorator(node) {
   if (ts.isClassDeclaration(node)) {
@@ -27,7 +31,7 @@ function findComponentDecorator(node) {
       if (ts.isCallExpression(expression)) {
         return expression.expression.text === 'Component';
       }
-      return false;
+      return null;
     });
   }
 }
@@ -41,6 +45,28 @@ function filterSupportedProps(componentDecorator) {
 
 function getPropsFromComponentDecorator(componentDecorator) {
   return componentDecorator.expression.arguments[0].properties;
+}
+
+function genImport(names) {
+  return ts.createImportDeclaration(
+    undefined,
+    undefined,
+    ts.createImportClause(
+      undefined,
+      ts.createNamedImports(
+        names.map((name) =>
+          ts.createImportSpecifier(undefined, ts.createIdentifier(name))
+        )
+      ),
+      false
+    ),
+    ts.createStringLiteral('vue-property-decorator')
+  );
+}
+
+function insertImportDeclaration(sourceFile, node, index) {
+  const l = index === -1 ? 0 : 1;
+  sourceFile.statements.splice(index === -1 ? 1 : index, l, node);
 }
 
 /**
@@ -59,7 +85,74 @@ function getPropsFromComponentDecorator(componentDecorator) {
  * })
  * @param {SourceFile} sourceFile
  */
-function beautify(sourceFile) {}
+function beautify(sourceFile) {
+  const names = {
+    Component: true,
+  };
+  const { statements } = sourceFile;
+  for(let i = 0, l = statements.length; i< l;i++){
+    const node = statements[0]
+  }
+  statements.forEach((node) => {
+    const componentDecorator = findComponentDecorator(node);
+    if (componentDecorator) {
+      const properties = getPropsFromComponentDecorator(componentDecorator);
+      const deletableNodes = [];
+
+      [...properties].forEach((prop, index) => {
+        if (ts.isPropertyAssignment(prop)) {
+          if (prop.initializer.properties.length === 0) {
+            deletableNodes.push(prop)
+          }
+        }
+      })
+      lodash.pullAll(properties, deletableNodes);
+    }
+    if (ts.isClassDeclaration(node)) {
+      const { decorators } = node;
+      // 找到被 Component 装饰器所在的 class
+      if (
+        decorators.some((node) => {
+          const { expression } = node;
+          if (ts.isCallExpression(expression)) {
+            return expression.expression.text === 'Component';
+          }
+          return false;
+        })
+      ) {
+        // 找到所有支持的装饰器名称
+        node.members.forEach((item) => {
+          const decoratorName =
+            item.decorators && item.decorators[0].expression.expression.text;
+          if (supportedDecoratorNames.includes(decoratorName)) {
+            names[decoratorName] = true;
+          }
+        });
+      }
+    }
+  });
+  // 找到vue-class-component的位置
+  const vueClassComponentIndex = statements.findIndex(
+    // import './store' 这种写法没有importClause
+    // import {aa,bb} from 'xx' 这种写法没有 importClause.name
+    (item) =>
+      ts.isImportDeclaration(item) &&
+      item.importClause &&
+      item.importClause.name &&
+      item.importClause.name.text === 'Component' &&
+      item.moduleSpecifier.text === 'vue-class-component'
+  );
+  if (vueClassComponentIndex !== -1) {
+    statements.splice(vueClassComponentIndex, 1);
+  }
+  const vuePropertyDecoratorIndex = statements.findIndex(
+    (item) =>
+      item.moduleSpecifier.text === 'vue-property-decorator'
+  )
+  // 将原有的 vue-property-decorator 替换为新的
+  insertImportDeclaration(sourceFile, genImport(Object.keys(names)), vuePropertyDecoratorIndex);
+
+}
 
 module.exports = function (sourceFile) {
   sourceFile.statements.forEach((node) => {
